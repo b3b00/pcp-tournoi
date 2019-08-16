57d75d3a6b0f9192839256dddc6df3492874ae3b
b3b00 <olivier.duhart@gmail.com>
2019-08-16 08:21:35 +0200

    fix group naming

--- src/main/java/org/pcp/tournament/service/GroupsStrategies.java
+++ src/main/java/org/pcp/tournament/service/GroupsStrategies.java
@@ -22,7 +22,7 @@ public class GroupsStrategies {
         }
         try {
         for (int i = 0; i < groupNumber; i++) {
-            String name = Character.toString ((char) i+65);
+            String name = Character.toString ((char) (i+65));
             Group group = new Group(name);            
             for (int j = 0; j < baseTeamNumber; j++) {
                 int randomIndex = rnd.nextInt(indexes.size());
