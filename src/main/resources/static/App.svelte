2f3266ea70596ee7be018c90593ddb6c4b8da7f2
b3b00 <olivier.duhart@gmail.com>
2019-08-19 10:19:13 +0200

    refacto : components spliting build/run

--- src/main/resources/static/App.svelte
+++ src/main/resources/static/App.svelte
@@ -1,64 +1,11 @@
 <script>
 
-const states = {
-	CONFIG : "config",
-	PLAYERS : "players",
-	TEAMS : "teams",
-	GROUPS : "groups"
-}
+
+ 	import Build from './components/build/Build.svelte';
 
 
-	// layout
-	import Layout from './components/layout/Layout.svelte';
-
-	// screens
-	import Config from './components/config.svelte';
-	import Players from './components/players.svelte';
-	import Teams from './components/teams.svelte';
-	import Groups from './components/groups.svelte';
-
-	
-		
-	let state=states.CONFIG;
-	let tournamentId = -1;
-
-	function onConfig(data) {
-		tournamentId = data.detail.tournamentId;
-		state=states.CONFIG;
-	}
-
-	function onPlayers(data) {
-		tournamentId = data.detail.tournamentId;
-		state= states.PLAYERS;
-	}
-
-	function onTeams(data) {
-		tournamentId = data.detail.tournamentId;
-		state= states.TEAMS;
-	}
-
-
-	function onGroups(data) {
-		tournamentId = data.detail.tournamentId;
-		state= states.GROUPS;
-	}
-
-	function setTournament(data) {
-		tournamentId = data.detail.tournamentId;
-	}
 
 </script>
 
-<Layout on:one="{onConfig}" on:two="{onPlayers}" on:three="{onTeams}" on:four="{onGroups}" tournamentId={tournamentId}>
-	{#if (state == states.CONFIG) }
-		<Config ournamentId={tournamentId} on:setTournament={setTournament}/>
-	{:else if (state == states.PLAYERS)}
-		<Players tournamentId={tournamentId} />
-	{:else if (state == states.TEAMS)}	
-		<Teams tournamentId={tournamentId} />
-	{:else if (state == states.GROUPS)}
-		<Groups tournamentId={tournamentId} />
-	{:else}
-		<p>unknwon state <strong>{state}</strong></p>
-	{/if}
-</Layout>
+
+<Build></Build>
---
+++ src/main/resources/static/components/build/Build.svelte
@@ -0,0 +1,65 @@
+<script>
+
+    const states = {
+        CONFIG : "config",
+        PLAYERS : "players",
+        TEAMS : "teams",
+        GROUPS : "groups"
+    }
+    
+    
+        // layout
+        import Layout from './layout/Layout.svelte';
+    
+        // screens
+        import Config from './config.svelte';
+        import Players from './players.svelte';
+        import Teams from './teams.svelte';
+        import Groups from './groups.svelte';
+    
+        
+            
+        let state=states.CONFIG;
+        let tournamentId = -1;
+    
+        function onConfig(data) {
+            tournamentId = data.detail.tournamentId;
+            state=states.CONFIG;
+        }
+    
+        function onPlayers(data) {
+            tournamentId = data.detail.tournamentId;
+            state= states.PLAYERS;
+        }
+    
+        function onTeams(data) {
+            tournamentId = data.detail.tournamentId;
+            state= states.TEAMS;
+        }
+    
+    
+        function onGroups(data) {
+            tournamentId = data.detail.tournamentId;
+            state= states.GROUPS;
+        }
+    
+        function setTournament(data) {
+            tournamentId = data.detail.tournamentId;
+        }
+    
+    </script>
+    
+    <Layout on:one="{onConfig}" on:two="{onPlayers}" on:three="{onTeams}" on:four="{onGroups}" tournamentId={tournamentId}>
+        {#if (state == states.CONFIG) }
+            <Config ournamentId={tournamentId} on:setTournament={setTournament}/>
+        {:else if (state == states.PLAYERS)}
+            <Players tournamentId={tournamentId} />
+        {:else if (state == states.TEAMS)}	
+            <Teams tournamentId={tournamentId} />
+        {:else if (state == states.GROUPS)}
+            <Groups tournamentId={tournamentId} />
+        {:else}
+            <p>unknwon state <strong>{state}</strong></p>
+        {/if}
+    </Layout>
+    
