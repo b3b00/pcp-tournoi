c98dbb53aa54119de89de6fd0591e9a4eca8deea
b3b00 <olivier.duhart@gmail.com>
2019-08-19 11:14:23 +0200

    accueil

--- src/main/resources/static/App.svelte
+++ src/main/resources/static/App.svelte
@@ -3,7 +3,8 @@
 
 	const STATE = {
 		BUILD : "build",
-		RUN : "run"
+		RUN : "run",
+		HOME : "home"
 	}
 
 	import { onMount } from 'svelte';
@@ -18,7 +19,7 @@
 
 	let tournaments = [];
 
-	let state = STATE.BUILD;
+	let state = STATE.HOME;
 
 	onMount(async () => {
         fetchTournaments();
@@ -45,22 +46,26 @@
 <div class="w3-panel w3-card startDialog">
 
 		<p style="display:inline">
+				<button on:click={() => changeState(STATE.HOME)}>Accueil</button>
 				<button on:click={() => changeState(STATE.BUILD)}>Configuration</button>
 				<button on:click={() => changeState(STATE.RUN)}>Jeu</button>	 			
 			</p>
+</div>
 
+
+
+
+{#if state == STATE.HOME}
 
 <ul class="w3-ul w3-border">
-<!-- <li>
-<h2>Names</h2>
-</li> -->
 {#each tournaments as tournament}
 	<li class="w3-bar" style="cursor: pointer;" on:click={() => {setTournament(tournament.id);}}> 
 		{tournament.name}
 	</li>
 {/each}
 </ul>
-</div>
+
+{/if}
 
 
 {#if (state == STATE.BUILD)}
