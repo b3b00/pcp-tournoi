b50031a479b5d4ff4c8ef728b5d1b1310eed62d5
b3b00 <olivier.duhart@gmail.com>
2019-08-20 15:55:58 +0200

    match UI WIP

---
+++ src/main/resources/static/components/run/match.svelte
@@ -0,0 +1,87 @@
+<style>
+    td.teamName {
+        font-weight: bold;
+    }
+</style>
+<script>
+    import { onMount } from 'svelte';
+    import { createEventDispatcher } from 'svelte';
+
+    export let tournamentId;
+
+    export let tournament;
+
+    export let team1;
+
+    export let team2;
+
+    export let team1Id;
+
+    export let team2Id;
+
+    export let setCount = 1;
+
+    let scores = [];
+
+    function initScores() {
+        if (tournament.options !== undefined && tournament.options.winningSets > 1) {
+            setCount = 2 * tournament.options.winningSets - 1;
+        }
+        else {
+            setCount = 1;
+        }
+        console.log(`initialize scores for a ${setCount} sets` );
+        for (let i = 0; i < setCount; i++) {
+            scores.push({ left: "0", right: "1" });
+        }
+        scores = scores;
+    }
+
+    
+
+    onMount(async () => {
+        console.log("tournamentId : " + tournamentId);
+        console.log(team1);
+        console.log(team2);
+        console.log(tournament);
+        //initScores();
+    });
+
+
+    $:{
+        tournament = tournament;
+        if (tournament.options !== undefined && (scores == undefined || scores.length == 0)) {
+            initScores();
+        }
+    }
+
+</script>
+<div class="w3-container">
+
+    <table>
+        <tr>
+            <td></td>
+                {#each scores as score,i (score)}
+                <td align="center">{i}</td>
+                {/each}
+        </tr>
+        <tr>
+            <td class="teamName">{team1.name}</td>
+            {#each scores as score (score)}
+            <!---->
+            <td><input type="number" min="0" placeholder="0" bind:value={score.left} /></td>
+            {/each}
+        </tr>
+        <tr>
+            <td class="teamName">{team2.name}</td>
+            {#each scores as score (score)}
+            <!-- bind:value={score.right}--> 
+            <td><input type="number" min="0" placeholder="0" bind:value={score.right} /></td>
+            {/each}
+        </tr>
+    </table>
+    
+    {#each scores as score (score)}
+    <p>{score.left} - {score.right}</p>
+    {/each}
+</div>
--- src/main/resources/static/components/run/run.svelte
+++ src/main/resources/static/components/run/run.svelte
@@ -1,5 +1,35 @@
 <script>
 
+    import Match from './match.svelte';
+    import { onMount } from 'svelte';
+    import { createEventDispatcher } from 'svelte';
+
+    export let tournamentId;
+
+    let tournament = {};
+    let team1 = {};
+    let team2 = {};
+    let setCount = 1;
+
+    onMount(async () => {
+        if (tournamentId !== undefined && tournamentId !== null && tournamentId != -1) {
+            await fetchTournament(tournamentId);            
+        }
+        tournament = tournament;
+
+    });
+
+    async function fetchTournament(id) {
+        const res = await fetch(`/tournaments/${id}`);
+        tournament = await res.json();
+        tournamentId = tournament.id;
+        team1 = tournament.teams[0];
+        team2 = tournament.teams[1];
+    }
 
 </script>
 <p> coming ...</p>
+<Match tournament={tournament} team1={team1} team2={team2} >
+    
+</Match>
+
