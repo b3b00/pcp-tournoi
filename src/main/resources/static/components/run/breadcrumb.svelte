608853cb1ace1aae6b530e9416a8d669021ae222
b3b00 <olivier.duhart@gmail.com>
2019-08-22 15:38:23 +0200

    breadcrumb initial

---
+++ src/main/resources/static/components/run/breadcrumb.svelte
@@ -0,0 +1,22 @@
+<style>
+</style>
+
+<script>
+    import { onMount } from 'svelte';
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+    export let items = {}
+
+    function onClick(index) {
+        dispatch("click", { 'items': items.slice(0, index) });
+    }
+
+</script>
+
+{#each items as item,i}
+
+    <a href="#" on:click={() => {onClick(i)}}</a>}>{item.name}</a>
+
+{/each}
