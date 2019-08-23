b529067b90eba0d737ee968b8ca0a2d88a497551
b3b00 <olivier.duhart@gmail.com>
2019-08-23 16:09:54 +0200

    typo

--- src/main/resources/static/components/run/group_preview.svelte
+++ src/main/resources/static/components/run/group_preview.svelte
@@ -42,7 +42,7 @@
 
   <div class="w3-container w3-quarter" on:click={openGroup} >
 
-        <div class="w3-padding w3-border"><h4>group {groupPlay.group.name}</h4></div>
+        <div class="w3-padding w3-border"><h4>poule {groupPlay.group.name}</h4></div>
         {#each groupPlay.rankings as ranking} 
           <div class="w3-container w3-padding w3-border-right w3-border-left w3-border-bottom">
           <div class="w3-half" style="text-align: center">{ranking.team.name}</div>  
