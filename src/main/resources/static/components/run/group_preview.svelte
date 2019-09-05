f13348555ce15fdb09ba6ab820624bd0b56c7086
b3b00 <olivier.duhart@gmail.com>
2019-09-05 16:35:10 +0200

    flag ended groups

--- src/main/resources/static/components/run/group_preview.svelte
+++ src/main/resources/static/components/run/group_preview.svelte
@@ -35,6 +35,11 @@
     function openMatch() {
       
     }
+
+    function getIsDoneflag(groupPlay) {
+      let done =  "<span style='color:green' class='fa fa-check w3-display-center'></span>";
+      return groupPlay.isDone ? done : "";
+    }
   
   </script>
   
@@ -43,7 +48,7 @@
 
   <div class="w3-container w3-quarter" on:click={openGroup} >
 
-        <div class="w3-padding w3-border"><h4>poule {groupPlay.group.name}</h4></div>
+        <div class="w3-padding w3-border"><h4>poule {groupPlay.group.name} {@html getIsDoneflag(groupPlay)}</h4></div>
         {#each groupPlay.rankings as ranking} 
           <div class="w3-container w3-padding w3-border-right w3-border-left w3-border-bottom">
           <div class="w3-half" style="text-align: center">{@html GroupDisplay.getTeamDisplayName(ranking.team,null)}</div>  
