f9c83fc36c570bc57fc0eda7ade410a58a8e4ca4
b3b00 <olivier.duhart@gmail.com>
2019-10-04 17:34:24 +0200

    players download UT

--- src/test/java/org/pcp/tournament/PlayerUploadTests.java
+++ src/test/java/org/pcp/tournament/PlayerUploadTests.java
@@ -6,12 +6,20 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import java.io.File;
+import java.io.Reader;
+import java.io.StringReader;
 import java.util.List;
+
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 
 import org.junit.Test;
 
 import org.junit.runner.RunWith;
 import org.pcp.tournament.dao.OptionsDao;
+import org.pcp.tournament.dao.PlayerDao;
 import org.pcp.tournament.dao.TournamentDao;
 import org.pcp.tournament.model.*;
 import org.pcp.tournament.service.PlayersService;
@@ -34,6 +42,9 @@ public class PlayerUploadTests {
     @Autowired
     OptionsDao optionsDao;
 
+    @Autowired
+    PlayerDao playerDao;
+
     @Test
     @Transactional
     public void testPlayersUpload() {
@@ -55,4 +66,54 @@ public class PlayerUploadTests {
         }
     }
 
+    @Test
+    @Transactional
+    public void testPlayersDownload() {
+        try {
+            Options options = optionsDao.findByMode(Mode.DOUBLE);
+            Tournament tournament = new Tournament("testingrun");
+            tournament.setOptions(options);
+            tournament = tournamentDao.save(tournament);    
+            Player player = new Player("1",true);
+            player.setTournament(tournament);
+            player = playerDao.save(player);
+            tournament.addPlayer(player);
+            tournament = tournamentDao.save(tournament);
+            player = new Player("2",false);
+            player.setTournament(tournament);
+            player = playerDao.save(player);
+            tournament.addPlayer(player);
+            tournament = tournamentDao.save(tournament);
+
+            String csv = playersService.playersToCSV(tournament);
+
+            Reader reader = new StringReader(csv);
+
+            CSVParser parser = new CSVParserBuilder()
+                .withSeparator(';')
+                .withIgnoreQuotations(true)
+                .build();
+
+            CSVReader csvReader = new CSVReaderBuilder(reader)
+                .withSkipLines(0)
+                .withCSVParser(parser)
+                .build();
+
+            List<String[]> lines = csvReader.readAll();
+            assertEquals(2, lines.size());
+            String[] l1 = lines.get(0); 
+            assertEquals(2, l1.length);
+            assertEquals("1", l1[0]);
+            assertEquals("*", l1[1]);
+            String[] l2 = lines.get(1); 
+            assertEquals(2, l2.length);
+            assertEquals("2", l2[0]);
+            assertEquals("", l2[1]);
+
+        }
+        catch(Exception e) {
+            fail(e.getMessage());
+        }
+
+    }
 }
